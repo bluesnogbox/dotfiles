e728e0522bd8fc4be5f221bb57cd43bd0c76b7bf
bluesnogbox <mybluesnogbox@gmail.com>
2015-07-26 01:12:23 -0400

    edited script

--- display-manager.sh
+++ display-manager.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+
+#while [ "$i" != ^[123] ]; do
+echo
 echo -e "Choose your environment:"
-echo " "
+echo 
 echo "1) i3"
 echo "2) Default"
 echo "3) TTY Console"
-echo " "
-read i
-if [ "$i" == "1" ]; then
-	startx /home/taylor/.xinitrc-i3
-elif [ "$i" == "2" ]; then
-	startx /home/taylor/.xinitrc-manjaro
-elif [ "$i" == "3" ]; then
-	archey3
+echo 
+read -n 1 -r i
+if [[ "$i" =~ ^[123]$ ]]; then
+  if [ "$i" == "1" ]; then
+    startx /home/taylor/.xinitrc-i3
+  elif [ "$i" == "2" ]; then
+    startx /home/taylor/.xinitrc-manjaro
+  elif [ "$i" == "3" ]; then
+    archey3
+  fi
 else
-	echo "Not a valid choice!"
+  echo
+  echo
+  echo -e "Not a valid choice!"
 fi
-echo " "
+echo 
+#done
