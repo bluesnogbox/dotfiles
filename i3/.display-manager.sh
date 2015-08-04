781d4e479e4e7a422ba09cd46784f01cb88f0fab
sn0ggle <mybluesnogbox@gmail.com>
2015-08-04 13:01:32 -0400

    cleanup

---
+++ i3/.display-manager.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#while [ "$i" != ^[123] ]; do
+if [ ! -z $TMUX ]; then
+  archey3
+else
+
+echo
+echo -e "Choose your environment:"
+echo 
+echo "1) i3"
+echo "2) Default"
+echo "3) TTY Console"
+echo 
+read -n 1 -r i
+if [[ "$i" =~ ^[123]$ ]]; then
+  if [ "$i" == "1" ]; then
+    startx /home/taylor/.xinitrc-i3
+  elif [ "$i" == "2" ]; then
+    startx /home/taylor/.xinitrc-manjaro
+  elif [ "$i" == "3" ]; then
+    archey3
+  fi
+else
+  echo
+  echo
+  echo -e "Not a valid choice!"
+fi
+echo
+fi
+#done
